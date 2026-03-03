77d7d31d21a9bd17f3ede888e2c1f3626a345236
Zood <jramirezlopez03@gmail.com>
2026-03-02 19:39:47 -0700

    Se movio el photogallery donde esta el diorama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
       <Hero />
       <LudoraCardsSection />
       <BlockyFinalSection />
-      <MinecraftDiorama />
-      <MethodologyStack />
       <PhotoGallery />
+      <MethodologyStack />
+      <MinecraftDiorama />
       <Footer />
     </main>
   );
